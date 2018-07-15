a2af752bb33e5028ab803fb42a5a64be4673d578
SmallwoodRaine <36510793+SmallwoodRaine@users.noreply.github.com>
2018-07-15 14:33:39 -0700

    Added boundaries and projectile spawn

--- ball.py
+++ ball.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 # with as many balls as you can spawn
 # on the screen.
 # The score will be based on amount of balls and time spent.
+# I will be making functions for all the if statements.
 pygame.init()
 
 class Player:
@@ -29,9 +30,12 @@ DH = 600
 INTIALPOSITION = (DW/2,DH/2 )
 WHITE = (255, 255, 255)
 BLACK = (0, 0 ,0)
+myFont = pygame.font.SysFont("Times New Roman", 18)
+randNumLabel = myFont.render("You have rolled:", 1, BLACK)
+diceDisplay = myFont.render(str(DW), 1, BLACK)
+
 GAMEDISPLAY = pygame.display.set_mode((DW, DH))
-BALLVELOCITYx = 1
-BALLVELOCITYy = 1
+BALLVELOCITY = 1
 pygame.display.set_caption('Ball')
 
 clock = pygame.time.Clock()
@@ -42,6 +46,7 @@ def createRock(x, y, direction):
 	rock = Projectile(x, y, direction)
 	return rock
 
+
 #start player at middle of screen
 rocks = []
 ball = Player(INTIALPOSITION[0], INTIALPOSITION[1])	
@@ -49,11 +54,9 @@ xDiff = 0
 yDiff = 0
 while True:
 	for event in pygame.event.get():
-		
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
-		
 		# movement 
 		if event.type == KEYDOWN:
 			if event.key == K_LEFT:
@@ -67,29 +70,61 @@ while True:
 			
 	#shooting projectiles		
 			if event.key == K_a:
-				rocks.append(createRock(ball.x, ball.y, LEFT))
-			if event.key == K_RIGHT:
-				xDiff = 5
-			if event.key == K_DOWN:
-				yDiff = -5
-			if event.key == K_UP:	
-				yDiff = 5
+				rocks.append(createRock(ball.x+13, ball.y+13, LEFT))
+			elif event.key == K_d:
+				rocks.append(createRock(ball.x+13, ball.y+13, RIGHT))
+			elif event.key == K_s:
+				rocks.append(createRock(ball.x+13, ball.y+13, DOWN))
+			elif event.key == K_w:	
+				rocks.append(createRock(ball.x+13, ball.y+13, UP))
 
-		if event.type ==KEYUP:
-		  	if event.key == K_DOWN or event.key == K_UP: 
-		  		yDiff = 0
-		  	if event.key == K_RIGHT or event.key == K_LEFT:
-		  		xDiff = 0
-		  	if event.key == K_SPACE:
-		  		xDiff = 0
-		  		yDiff = 0
+		if event.type == KEYUP:
+		  	if event.key == K_DOWN:
+		  		yDiff = -5
+		  	if event.key == K_UP: 
+		  		yDiff = 5
+		  	if event.key == K_RIGHT:
+		  		xDiff = 5	 
+		  	if event.key == K_LEFT:
+		  		xDiff = -5
 	ball.x += xDiff
 	ball.y -= yDiff
+
 	GAMEDISPLAY.fill(WHITE)
-	for i in rocks:
+	GAMEDISPLAY.blit(randNumLabel, (520, 20))
+	GAMEDISPLAY.blit(diceDisplay, (520, 30))
+
+	if ball.x < 0:
+		ball.x = 0
+	if ball.x > DW:
+		ball.x = DW
+	if ball.y < 0:
+		ball.y = 0
+	if ball.y > DH:
+		ball.y = DH
+
+	# make all if statements a function to clean up
+	for i in rocks:	
+		if i.x == 0:
+			i.direction = RIGHT
+		if i.x == DW:
+			i.direction = LEFT
+		if i.y == 0:
+			i.direction = DOWN
+		if i.y == DH:
+			i.direction = UP
+
 		if i.direction == LEFT:
-			i.x -= BALLVELOCITYx
-		pygame.draw.circle(GAMEDISPLAY, BLACK, (int(i.x), int(i.y)), 5, 0)					
+			i.x -= BALLVELOCITY
+		if i.direction == RIGHT:
+			i.x += BALLVELOCITY
+		if i.direction == UP:
+			i.y -= BALLVELOCITY
+		if i.direction == DOWN:
+			i.y += BALLVELOCITY	
+		if int(i.x) == int(ball.x): #and int(i.y) == int(ball.y):
+			pygame.quit()
+		pygame.draw.circle(GAMEDISPLAY, BLACK, (int(i.x), int(i.y)), 5, 0)
 	pygame.draw.circle(GAMEDISPLAY, BLACK, (int(ball.x), int(ball.y)), 20, 0)
 	clock.tick(30)
 	pygame.display.update()
